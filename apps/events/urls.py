5019ce8cfec614c61a2256793d9326318fe99d60
Enio Jr <eniojr100@gmail.com>
2026-04-11 19:08:18 -0300

    :sparkles: feat(events): cria views de cancelamento e dashboard do organizador

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', EventoSocialListCreateView.as_view(), name='evento-list-create'),
     path('<int:pk>/', EventoSocialDetailView.as_view(), name='evento-detail'),
     path('inscricoes/', views.InscreverEventoView.as_view(), name='inscrever-evento'),
-    path('minhas-inscricoes/', views.MinhasInscricoesView.as_view(), name='minhas-inscricoes')
+    path('minhas-inscricoes/', views.MinhasInscricoesView.as_view(), name='minhas-inscricoes'),
+    path('inscricoes/<int:pk>/cancelar/', views.CancelarInscricaoView.as_view(), name='cancelar-inscricao'),
+    path('inscricoes-recebidas/', views.InscricoesRecebidasView.as_view(), name='inscricoes-recebidas')
 ]
--- apps/events/views.py
+++ apps/events/views.py
@@ -1,10 +1,13 @@
-from rest_framework import generics, filters, generics
+from rest_framework import generics, filters, generics,status
+from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import EventoSocial, Inscricao
 from .serializers import EventoSocialSerializer, InscricaoSerializer
 from django.contrib.gis.geos import Point
 from django.contrib.gis.measure import D
 from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
 
 
 class EventoSocialListCreateView(generics.ListCreateAPIView):
@@ -48,3 +51,24 @@ class MinhasInscricoesView(generics.ListAPIView):
 
     def get_queryset(self):
         return Inscricao.objects.filter(participante=self.request.user)
+    
+
+class CancelarInscricaoView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def patch(self, request, pk):
+        
+        inscricao = get_object_or_404(Inscricao, id=pk, participante=self.request.user)
+
+        inscricao.status = 'cancelada'
+        inscricao.save()
+
+        return Response({"mensagem": "Inscrição cancelada com sucesso!"}, status=status.HTTP_200_OK)
+    
+
+class InscricoesRecebidasView(generics.ListAPIView):
+    serializer_class = InscricaoSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Inscricao.objects.filter(evento__organizador=self.request.user)
