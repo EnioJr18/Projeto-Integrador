62682b14b83e3e6f61b7e4eb6e064481460d14f9
Guilherme Pontes <guilhermepontespinto@gmail.com>
2026-04-18 18:06:54 -0300

    fix(events): corrige erro de re-inscricao e gerencia vagas

--- apps/events/serializers.py
+++ apps/events/serializers.py
@@ -1,14 +1,10 @@
 from rest_framework import serializers
-from .models import EventoSocial
-from .models import Inscricao
-
+from .models import EventoSocial, Inscricao
 
 class EventoSocialSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = EventoSocial
         fields = '__all__'
-
 
 class InscricaoSerializer(serializers.ModelSerializer):
     participante = serializers.HiddenField(
@@ -19,3 +15,29 @@ class InscricaoSerializer(serializers.ModelSerializer):
         model = Inscricao
         fields = ['id', 'evento', 'participante', 'status', 'data_inscricao']
         read_only_fields = ['status', 'data_inscricao']
+        
+        validators = []
+
+    def create(self, validated_data):
+        evento = validated_data['evento']
+        participante = validated_data['participante']
+        
+        inscricoes_confirmadas = Inscricao.objects.filter(
+            evento=evento, 
+            status='confirmada'
+        ).count()
+
+        novo_status = 'pendente' if inscricoes_confirmadas >= evento.vagas else 'confirmada'
+
+        inscricao_existente = Inscricao.objects.filter(evento=evento, participante=participante).first()
+
+        if inscricao_existente:
+            if inscricao_existente.status == 'cancelada':
+                inscricao_existente.status = novo_status
+                inscricao_existente.save()
+                return inscricao_existente
+            else:
+                raise serializers.ValidationError({"detalhe": "Já estás inscrito neste evento."})
+
+        validated_data['status'] = novo_status
+        return super().create(validated_data)
