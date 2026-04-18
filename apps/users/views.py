4661f99ec1bed71db4ca0a7f4adba251fdabab32
Guilherme Pontes <guilhermepontespinto@gmail.com>
2026-04-18 18:05:22 -0300

    feat(users): adiciona rota de perfil do usuario logado

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,9 +1,16 @@
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from .models import CustomUser
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, UserSerializer
 
 class RegisterView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()    
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
+class PerfilView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
