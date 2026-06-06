ac14452a2f05f2594500f02dcbcc888febe33ca5
Enio Jr <eniojr100@gmail.com>
2026-06-05 23:50:00 -0300

    Adicionando script temporario para reset de senha

--- back/reset_senha.py
+++ back/reset_senha.py
@@ -10,8 +10,8 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 # Altere para o seu nome de usuário do admin e a senha que você deseja colocar
-USERNAME = 'Enio' 
-NOVA_SENHA = 'Enio123'
+USERNAME = 'Enio'  # Substitua pelo nome de usuário do admin que deseja resetar
+NOVA_SENHA = 'Eniojr123'
 
 try:
     user = User.objects.get(username=USERNAME)
