b4aabccad8f114d97068007bc29e49122f180707
Enio Jr <eniojr100@gmail.com>
2026-06-06 00:37:54 -0300

    Registrando User no admin e removendo rota de emergencia

--- back/apps/events/admin.py
+++ back/apps/events/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import EventoSocial, Inscricao
+from django.contrib.auth import get_user_model
 
 # Fazendo a mágica acontecer no painel
 admin.site.register(EventoSocial)
 admin.site.register(Inscricao)
+User = get_user_model()
+admin.site.register(User)
